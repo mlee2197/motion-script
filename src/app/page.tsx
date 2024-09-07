1af0a647fad55bafdaefd5e3f925aae0b3750ea1
Matt <mlee138@binghamton.edu>
2024-09-07 14:44:05 -0400

    update footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,12 @@ export default function Home() {
     <div className="max-w-[1280px] mx-auto">
       <main>
         <Hero />
+        {/* <h2>
+          30 days of animations. 30 days of fun.
+        </h2> */}
       </main>
-      <footer className="flex flex-col gap-6 md:flex-row ">
+      <footer className="flex gap-6 container-padding-x container-padding-y">
+        {/* TODO: update to use icons */}
         <a href="https://github.com/mattlee007/animation-suite">GitHub</a>
         <a href="https://www.linkedin.com/in/matthewlee1297/">Linkedin</a>
         <a href="mailto:mlee2197@gmail.com">Email</a>
