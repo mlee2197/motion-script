f3049a756a26a56e551eb62159679007dd75b6fc
Matt <mlee138@binghamton.edu>
2024-09-08 11:16:30 -0400

    polish hero animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import Hero from "./Hero";
+import Hero from "../components/Hero";
 
 export default function Home() {
   return (
     <div className="max-w-[1280px] mx-auto">
       <main>
         <Hero />
-        {/* <h2>
-          30 days of animations. 30 days of fun.
-        </h2> */}
+        <h2 className="text-center text-xl mt-8 mb-12">
+          30 Days, 30 Animations: A Daily Showcase of Web Animation Creativity
+        </h2>
       </main>
       <footer className="flex gap-6 container-padding-x container-padding-y">
         {/* TODO: update to use icons */}
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,56 +4,79 @@ import { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
 import useIsMobile from "@/hooks/useIsMobile";
 import clsx from "clsx";
+import { shuffleArray } from "@/misc/utils";
 
 const HEADER_COLORS = ["#E6974E", "#E64E4E", "#FFFFFF"];
 
 const Hero = () => {
   const containerRef = useRef<HTMLDivElement>(null);
   const isMobile = useIsMobile();
-  const [mouse, setMouse] = useState({
-    x: 0,
-    y: 0,
-  });
+  const [mouse, setMouse] = useState({ x: 0, y: 0 });
   const [initialAnimation, setInitialAnimation] = useState(true);
+  const [particles, setParticles] = useState<{
+    left: { x: number; y: number }[];
+    right: { x: number; y: number }[];
+  }>({ left: [], right: [] });
+
+  useEffect(() => {
+    const numParticles = 30;
+    const generateParticles = () => {
+      const particles = [];
+      const maxWidth = Math.min(window.innerWidth * 1.25, 1440);
+      const maxHeight = Math.min(window.innerHeight / 1.5, 664 / 2);
+
+      for (let i = 0; i < numParticles; i++) {
+        const angle =
+          Math.random() * Math.PI + (Math.random() < 0.5 ? 0 : Math.PI);
+        const x = Math.cos(angle) * maxWidth;
+        const y = (Math.random() > 0.5 ? -1 : 1) * maxHeight;
+
+        particles.push({ x, y });
+      }
+
+      // Separate particles into left and right groups
+      const leftParticles = shuffleArray(particles);
+      const rightParticles = shuffleArray(particles);
+
+      setParticles({ left: leftParticles, right: rightParticles });
+    };
+
+    generateParticles();
+  }, []);
 
   // particles animation
   useEffect(() => {
-    const particleDuration = 6;
     const ctx = gsap.context(() => {
-      const particles = gsap.utils.toArray(".particle") as HTMLElement[];
-      particles.forEach((particle: HTMLElement, index: number) => {
-        const randomX =
-          Math.random() < 0.75
-            ? Math.max(0.25, Math.random()) * Math.min(window.innerWidth, 1280)
-            : Math.min(0.25, Math.random()) *
-              Math.min(window.innerWidth, 1280) *
-              -1;
-        const randomY =
-          Math.random() *
-          Math.min(window.innerHeight / 1.5, 664 / 2) *
-          (Math.random() < 0.5 ? -1 : 1);
+      const leftParticleElements = gsap.utils.toArray(
+        ".left-particle"
+      ) as HTMLElement[];
+      const rightParticleElements = gsap.utils.toArray(
+        ".right-particle"
+      ) as HTMLElement[];
+
+      const particleDuration = 6;
+      const stagger = 0.25;
+
+      const animateParticles = (particles: HTMLElement[]) => {
         const tl = gsap.timeline({ repeat: -1 });
-        tl.from(particle, {
-          opacity: 0,
+        tl.to(particles, {
+          opacity: 1,
           duration: particleDuration,
-          x: randomX,
-          y: randomY,
-          scale: 1,
-          ease: "power2.out",
-          delay: index * 0.5,
+          x: 0,
+          y: 0,
+          scale: 0,
+          ease: "circ.out",
+          stagger: stagger,
         });
-        tl.to(
-          particle,
-          {
-            opacity: 0,
-            duration: particleDuration / 2,
-          },
-          "<50%"
-        );
+      };
+
+      animateParticles(rightParticleElements);
+      gsap.delayedCall(rightParticleElements.length * stagger, () => {
+        animateParticles(leftParticleElements);
       });
     }, containerRef);
     return () => ctx.revert();
-  }, []);
+  }, [particles]);
 
   // Get mouse position for header animation
   useEffect(() => {
@@ -64,7 +87,6 @@ const Hero = () => {
       const { left, top, width, height } = container.getBoundingClientRect();
       const x = (clientX - left - width / 2) / 25;
       const y = (clientY - top - height / 2) / 25;
-      console.log(x, y);
       setMouse({ x, y });
     };
 
@@ -126,6 +148,26 @@ const Hero = () => {
       ref={containerRef}
       className="relative h-screen max-h-[664px] w-full overflow-hidden"
     >
+      {particles.left.map((particle, index) => (
+        <div
+          key={`left-${index}-particle`}
+          className={clsx(
+            "particle left-particle",
+            index % 3 === 0 ? "plus" : index % 3 === 1 ? "box" : "circle"
+          )}
+          style={{ translate: `${particle.x}px ${particle.y}px` }}
+        />
+      ))}
+      {particles.right.map((particle, index) => (
+        <div
+          key={`right-${index}-particle`}
+          className={clsx(
+            "particle right-particle",
+            index % 3 === 0 ? "plus" : index % 3 === 1 ? "box" : "circle"
+          )}
+          style={{ translate: `${particle.x}px ${particle.y}px` }}
+        />
+      ))}
       {HEADER_COLORS.map((color, index) => (
         <h1
           key={color}
@@ -147,15 +189,6 @@ const Hero = () => {
       >
         best experience on desktop
       </h2>
-      {Array.from({ length: 30 }).map((_, index) => (
-        <div
-          key={`${index}-particle`}
-          className={clsx(
-            "particle",
-            index % 3 === 0 ? "plus" : index % 3 === 1 ? "box" : "circle"
-          )}
-        />
-      ))}
     </div>
   );
 };
