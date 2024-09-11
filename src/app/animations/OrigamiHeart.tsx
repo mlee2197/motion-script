a8a1768f4e5d6551828e737c22c30eb180ef8511
Matthew Lee <mlee2197@gmail.com>
2024-09-11 10:10:38 -0400

    finish origami folding

--- src/app/animations/OrigamiHeart.tsx
+++ src/app/animations/OrigamiHeart.tsx
@@ -15,62 +15,96 @@ const OrigamiHeart = () => {
     const rightBox = ref.current.querySelector(".r-box");
     const tlTriangle = ref.current.querySelector(".tl-triangle");
     const trTriangle = ref.current.querySelector(".tr-triangle");
+    const bBoxContainer = ref.current.querySelector(".b-box-container");
     const bBox = ref.current.querySelector(".b-box");
+
     const brTriangle = ref.current.querySelector(".br-triangle");
     const blTriangle = ref.current.querySelector(".bl-triangle");
+    const duration = 1.5;
+    tl.set([topTriangle, leftBox, rightBox, tlTriangle, trTriangle, bBoxContainer, bBox, brTriangle, blTriangle], {
+      clearProps: true,
+    });
 
-    tl.fromTo(topTriangle, {
-      rotateY: 0,
-    }, {
+    tl.to(topTriangle, {
       rotateY: 180,
       delay: 0.5,
+      ease: "power1.inOut",
+      duration: duration,
     });
-    tl.fromTo(blTriangle, {
-      rotateY: 0,
-    }, {
-      rotateY: 180,
-      duration: 3,
+    tl.to(topTriangle, {
+      borderRightColor: "red",
+      duration: 0,
+    }, "<+50%")
 
+    tl.to([blTriangle, brTriangle, bBoxContainer], {
+      rotateY: 180,
+      duration: duration,
+      ease: "power1.inOut",
     });
-    tl.fromTo(brTriangle, {
-      rotateY: 0,
-    }, {
-      rotateY: 180,
-      duration: 3,
+    tl.to([blTriangle, brTriangle], {
+      borderLeftColor: "red",
+      duration: 0,
+    }, "<+50%")
+    tl.to(bBox, {
+      backgroundColor: "red",
+      duration: 0,
     }, "<");
-    tl.fromTo(bBox, {
 
-      // transformOrigin: "top",
-      rotateY: 0,
-    }, {
+    // tl.to([trTriangle, tlTriangle], {
+    //   borderLeftColor: "red",
+    //   borderTopColor: "red",
+    //   zIndex: 5,
+    //   duration: 0,
+    // });
+    tl.to([trTriangle, tlTriangle], {
+      borderLeftColor: "red",
+      borderTopColor: "red",
+      duration: 0,
+    });
+    tl.to([blTriangle, brTriangle], {
+      display: "none",
+      duration: 0,
+    });
+
+    tl.to([leftBox, tlTriangle], {
+      rotateX: 180,
+      duration: duration,
+      ease: "power1.inOut",
+    });
+    tl.to([trTriangle, rightBox], {
       rotateY: 180,
-      duration: 3,
+      duration: duration,
+      ease: "power1.inOut",
     }, "<");
+    tl.to([leftBox, rightBox], {
+      backgroundColor: "red",
+      duration: 0,
+    }, "<+50%");
+
+
+
+
 
   };
 
   return (
     <div className="animation-container flex justify-center items-center">
       <PlayButton onClick={handleClick} />
-      {/* <canvas ref={ref} width={150} height={150}>
+      <div ref={ref} className="relative w-[150px] h-[150px] rotate-45">
+        <div className="absolute top-0 left-0 w-[100px] h-[100px] bg-gradient-to-br from-transparent via-transparent via-[48%] to-[48%] to-red-200"></div>
+        <div className="t-triangle absolute -top-1/2 -left-1/2 -translate-x-[13px] translate-y-[55.5px] border-[68px] border-transparent border-r-red-200 origin-right rotate-45"></div>
 
-      </canvas> */}
-      <div ref={ref} className="relative w-[150px] h-[150px] bg-white/20">
-        <div className="absolute top-0 left-0 w-[100px] h-[100px] bg-gradient-to-br from-transparent via-transparent via-[38%] to-[38%] to-blue-400"></div>
-        <div className="t-triangle absolute -top-1/2 -left-1/2 translate-x-[5px] translate-y-[59.5px] border-[54px] border-transparent border-r-white origin-right rotate-45"></div>
+        <div className="l-box absolute bottom-0 left-0 h-[50px] w-[50px] bg-red-200 origin-top"></div>
+        <div className="r-box absolute top-0 right-0 h-[50px] w-[50px] bg-red-200 origin-left"></div>
 
-        <div className="l-box absolute bottom-0 left-0 h-[50px] w-[50px] bg-purple-400"></div>
-        <div className="r-box absolute top-0 right-0 h-[50px] w-[50px] bg-pink-400"></div>
+        <div className="tr-triangle absolute bottom-1/2 translate-y-1/2 -right-[0px] border-[25px] border-transparent border-l-red-200 border-t-red-200 origin-left"></div>
+        <div className="tl-triangle absolute bottom-0 right-1/2 translate-x-1/2 border-[25px] border-transparent border-l-red-200 border-t-red-200 origin-top"></div>
 
-        <div className="tr-triangle absolute bottom-[50px] right-0 translate-y-[10.5px] -translate-x-[25px] border-[35.5px] border-transparent border-r-orange-400 origin-right rotate-45"></div>
-        <div className="tl-triangle absolute bottom-0 right-[50px] translate-y-[10.5px] -translate-x-[25px] border-[35.5px] border-transparent border-r-orange-400 origin-right rotate-45"></div>
-
-        {/* <div className="b-box absolute -bottom-0 -right-0 h-[50px] w-[50px] bg-green-400 rotate-45 origin-top"></div> */}
-        <div className="b-box absolute bottom-[25px] -right-[0px] h-[50px] w-[50px] rotate-45 origin-left">
-          <div className="absolute bottom-0 -right-[10.5px] h-[50px] w-[50px] bg-green-400 rotate-45" />
+        <div className="b-box-container absolute bottom-[25px] -right-[0px] h-[50px] w-[50px] rotate-45 origin-left">
+          <div className="b-box absolute bottom-0 -right-[10.5px] h-[50px] w-[50px] bg-red-200 rotate-45" />
         </div>
-        <div className="bl-triangle absolute bottom-0 right-0 translate-y-[10.5px] -translate-x-[4px] border-[35.5px] border-transparent border-l-yellow-400 origin-left rotate-45"></div>
-        <div className="br-triangle absolute bottom-[50px] -right-[50px] translate-y-[10.5px] -translate-x-[4px] border-[35.5px] border-transparent border-l-yellow-400 origin-left rotate-45"></div>
+        <div className="bl-triangle absolute bottom-0 right-0 translate-y-[10.5px] -translate-x-[4px] border-[35.5px] border-transparent border-l-red-200 origin-left rotate-45"></div>
+        <div className="br-triangle absolute bottom-[50px] -right-[50px] translate-y-[10.5px] -translate-x-[4px] border-[35.5px] border-transparent border-l-red-200 origin-left rotate-45"></div>
       </div>
     </div>
   );
