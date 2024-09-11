6ffa3980910cc8253bb9e9e52a28dab268e98f0d
Matt <mlee138@binghamton.edu>
2024-09-11 11:04:23 -0400

    refactor play restart

--- src/app/animations/PlayRestart.tsx
+++ src/app/animations/PlayRestart.tsx
@@ -1,8 +1,8 @@
 "use client";
-import PlayButton from "@/components/PlayButton";
 import { useRef } from "react";
 import gsap from "gsap";
 import Image from "next/image";
+import PlayButton from "@/components/PlayButton";
 
 import Rocket from "@/assets/rocket.svg";
 import Moon from "@/assets/moon.svg";
@@ -19,35 +19,23 @@ const PlayRestart = () => {
     const triangleContainer = select(".triangleContainer");
     const moon = select(".moon");
     const duration = 2.5;
-    tl.fromTo(
-      triangle,
-      {
-        scale: 1,
-        y: 70,
-      },
-      {
-        scale: 0.5,
-        y: -40,
-        duration: duration,
-        delay: 0.5,
-      }
-    );
-    tl.fromTo(
+    tl.set([circle, triangle, triangleContainer, moon], { clearProps: true });
+    tl.to(triangle, {
+      scale: 0.5,
+      y: -40,
+      duration: duration,
+      delay: 0.5,
+    });
+    tl.to(
       triangleContainer,
-      {
-        rotate: 0,
-      },
       {
         rotate: 360,
         duration: duration,
       },
       "<"
     );
-    tl.fromTo(
+    tl.to(
       moon,
-      {
-        scale: 0.1,
-      },
       {
         scale: 1,
         duration: duration,
@@ -58,8 +46,6 @@ const PlayRestart = () => {
       circle,
       {
         transformOrigin: "center",
-        strokeDashoffset: 285,
-        strokeWidth: 30,
         scale: 0.1,
       },
       {
@@ -75,49 +61,41 @@ const PlayRestart = () => {
   return (
     <div className="animation-container">
       <PlayButton onClick={animatePlay} />
-      <div ref={ref} className="relative h-full w-full">
-        <svg viewBox="0 0 100 100" className="w-full h-full">
-          <defs>
-            <linearGradient
-              id="circleGradient"
-              x1="0%"
-              y1="0%"
-              x2="100%"
-              y2="0%"
-            >
-              <stop offset="0%" stopColor="#e5b346" />
-              <stop offset="90%" stopColor="#ed463a" />
-            </linearGradient>
-          </defs>
-          <circle
-            cx="100"
-            cy="100"
-            r="33"
-            fill="none"
-            stroke="url(#circleGradient)"
-            strokeWidth="10"
-            strokeDasharray={285}
-            strokeDashoffset={285}
-            strokeLinecap="round"
-            className="top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
-          />
-        </svg>
-        <Image
-          src={Moon}
-          alt="Moon"
-          className="moon absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 scale-[0.1] rounded-full shadow-[0_0_8px_8px_rgba(255,255,255,0.1)]"
-          width={80}
-          height={80}
+      <svg viewBox="0 0 100 100" className="w-full h-full">
+        <defs>
+          <linearGradient id="circleGradient" x1="0%" y1="0%" x2="100%" y2="0%">
+            <stop offset="0%" stopColor="#e5b346" />
+            <stop offset="90%" stopColor="#ed463a" />
+          </linearGradient>
+        </defs>
+        <circle
+          cx="100"
+          cy="100"
+          r="33"
+          fill="none"
+          stroke="url(#circleGradient)"
+          strokeWidth="30"
+          strokeDasharray={285}
+          strokeDashoffset={285}
+          strokeLinecap="round"
+          className="top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
         />
-        <div className="triangleContainer absolute w-full h-full top-0 left-0 ">
-          <Image
-            src={Rocket}
-            alt="Rocket"
-            className="triangle absolute bottom-1/2 left-1/2 -translate-x-1/2 translate-y-1/2 origin-bottom"
-            width={120}
-            height={120}
-          />
-        </div>
+      </svg>
+      <Image
+        src={Moon}
+        alt="Moon"
+        className="moon absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 scale-[0.1] rounded-full shadow-[0_0_8px_8px_rgba(255,255,255,0.1)]"
+        width={80}
+        height={80}
+      />
+      <div className="triangleContainer absolute w-full h-full top-0 left-0 ">
+        <Image
+          src={Rocket}
+          alt="Rocket"
+          className="triangle absolute bottom-1/2 left-1/2 -translate-x-1/2 translate-y-1/2 origin-bottom"
+          width={120}
+          height={120}
+        />
       </div>
     </div>
   );
