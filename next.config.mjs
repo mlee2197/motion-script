819e988dd67aeb49cd73dc3919bc470255a1a7cf
Matthew Lee <mlee2197@gmail.com>
2024-09-13 19:26:32 -0400

    add sound support

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(mp3|wav|ogg)$/i,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            publicPath: '/_next/static/audio/',
+            outputPath: 'static/audio/',
+            name: '[name].[hash].[ext]',
+            esModule: false,
+          },
+        },
+      ],
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
