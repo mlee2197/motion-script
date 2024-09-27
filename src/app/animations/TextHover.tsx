af6d5ba953d1f10a36a2133bc420f73c4685aa39
Matt <mlee138@binghamton.edu>
2024-09-27 18:25:19 -0400

    fix duplicate keys

--- src/app/animations/TextHover.tsx
+++ src/app/animations/TextHover.tsx
@@ -5,14 +5,14 @@ const TextHover = () => {
       <div className="relative h-10 w-full text-center overflow-hidden group">
         <p className="absolute top-0 w-full mx-auto font-shrikhand text-4xl">
           {WORD.map((letter, index) => (
-            <span key={letter} className="inline-block transition-transform duration-300 group-hover:translate-y-full" style={{ transitionDelay: `${index * 25}ms` }}>
+            <span key={`${letter}-${index}-visible`} className="inline-block transition-transform duration-300 group-hover:translate-y-full" style={{ transitionDelay: `${index * 25}ms` }}>
               {letter}
             </span>
           ))}
         </p>
         <p className="absolute -top-full w-full mx-auto font-shrikhand text-4xl">
           {WORD.map((letter, index) => (
-            <span key={letter} className="inline-block transition-transform duration-300 group-hover:translate-y-full text-pink-300" style={{ transitionDelay: `${index * 25}ms` }}>
+            <span key={`${letter}-${index}-hidden`} className="inline-block transition-transform duration-300 group-hover:translate-y-full text-pink-300" style={{ transitionDelay: `${index * 25}ms` }}>
               {letter}
             </span>
           ))}
